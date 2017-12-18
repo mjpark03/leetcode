08403db18474e2a143f39df3680b9b86bf1041f3
mjpark03 <mjpark03@gmail.com>
2017-12-19 06:55:06 +0900

    subarray-sum-equals-k-2

---
+++ subarray-sum-equals-k-2.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int subarraySum(int[] nums, int k) {
+        int count = 0, sum = 0;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1);
+        
+        for (int num: nums) {
+            sum += num;
+            
+            if (map.containsKey(sum - k))
+                count += map.get(sum - k);
+            
+            map.put(sum, map.getOrDefault(sum, 0) + 1);
+        }
+        
+        return count;
+    }
+}
